ffa68a9e207bf1d3610f9a106f0f1ddc44664f12
Martin Salazard <martin.der.aze@gmail.com>
2017-06-15 23:47:20 +0200

    applyEnvironmentScript now accepts file template as paramter

--- util/src/main/groovy/net/tetrakoopa/poignee/UtilPlugin.groovy
+++ util/src/main/groovy/net/tetrakoopa/poignee/UtilPlugin.groovy
@@ -7,6 +7,8 @@ import org.gradle.api.tasks.bundling.Zip
 
 class UtilPlugin implements Plugin<Project> {
 
+	public static String ENVIRONMENT_PLACEHOLDER = "{environment}"
+
 	void apply(Project project) {
 		project.ext.getGitVersionName = { ->
 			def stdout = new ByteArrayOutputStream()
@@ -18,14 +20,25 @@ class UtilPlugin implements Plugin<Project> {
 		}
 
 		project.ext.applyEnvironmentScript = { ->
+			project.applyEnvironmentScript(null)
+		}
+		project.ext.applyEnvironmentScript = { template ->
+			String envScriptTemplate
+			if ( template != null ) {
+				if (!template.contains(ENVIRONMENT_PLACEHOLDER)) throw new GradleException("Template filename must contains '${ENVIRONMENT_PLACEHOLDER}'.")
+				envScriptTemplate = template
+			} else {
+				envScriptTemplate = "gradle/{environment}-env.gradle"
+			}
 			def env = 'dev'
 			if ( project.hasProperty('environment') ) {
 				env = project.environment
-				project.logger.info("No environment defined : assuming development environment")
+				project.logger.info("No environment defined : assuming development environment ('${env}')")
 			}
-			def envScript = project.file("gradle/${env}-env.gradle")
+			def envScriptName = envScriptTemplate.replace(ENVIRONMENT_PLACEHOLDER,"${env}")
+			def envScript = project.file(envScriptName)
 			if (!envScript.exists()) {
-				throw new GradleException("Unknown environment: '${env}'")
+				throw new GradleException("Environment config for '${env}' not found. Searched in '${envScript}'")
 			}
 			project.apply from: envScript
 		}
