eba031d87e4fdc469e48dedf3fe0541c4fbebee8
Martin Salazard <martin.der.aze@gmail.com>
2017-07-10 18:42:07 +0200

    small refactor

--- util/src/main/groovy/net/tetrakoopa/poignee/UtilPlugin.groovy
+++ util/src/main/groovy/net/tetrakoopa/poignee/UtilPlugin.groovy
@@ -8,14 +8,21 @@ class UtilPlugin implements Plugin<Project> {
 
 	public static String ENVIRONMENT_PLACEHOLDER = "{environment}"
 
+	public static String DEFAULT_ENVIRONMENT_FILENAME_TEMPLATE = "gradle/"+ENVIRONMENT_PLACEHOLDER+"-env.gradle"
+
+	private static String getGitVersionName(Project project) {
+		def stdout = new ByteArrayOutputStream()
+		project.exec {
+			commandLine 'git', 'describe', '--tags', '--always'
+			standardOutput = stdout
+		}
+		return stdout.toString().trim()
+	}
+
+
 	void apply(Project project) {
 		project.ext.getGitVersionName = { ->
-			def stdout = new ByteArrayOutputStream()
-			project.exec {
-				commandLine 'git', 'describe', '--tags', '--always'
-				standardOutput = stdout
-			}
-			return stdout.toString().trim()
+			return UtilPlugin.getGitVersionName(project)
 		}
 
 		project.ext.applyEnvironmentScript = { ->
@@ -27,7 +34,7 @@ class UtilPlugin implements Plugin<Project> {
 				if (!template.contains(ENVIRONMENT_PLACEHOLDER)) throw new GradleException("Template filename must contains '${ENVIRONMENT_PLACEHOLDER}'.")
 				envScriptTemplate = template
 			} else {
-				envScriptTemplate = "gradle/{environment}-env.gradle"
+				envScriptTemplate = DEFAULT_ENVIRONMENT_FILENAME_TEMPLATE
 			}
 			def env = 'dev'
 			if ( project.hasProperty('environment') ) {
